038dfad39f96cc43eb3f2a2d594e47a8039b46d8
Ken Tay <taykenneth@rocketmail.com>
2024-05-01 21:53:53 +1000

    first push

---
+++ Credit.cs
@@ -0,0 +1,13 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building...'
+                echo 'fetch the source code from the directory path specified by the environment variable.'
+                echo 'compile the code and generate any necessary artifacts.'
+            }
+        }
+    }
+}
